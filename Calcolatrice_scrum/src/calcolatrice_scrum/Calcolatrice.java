0e7d351a38233264d2355f146ee3d8f0f529ea51
MattiaSanzari <mattiasanzari2003@gmail.com>
2026-04-04 16:38:20 +0200

    Aggiunte le funzionaltà trigonometriche più conversione gradi radianti, radianti gradi.

--- Calcolatrice_scrum/src/calcolatrice_scrum/Calcolatrice.java
+++ Calcolatrice_scrum/src/calcolatrice_scrum/Calcolatrice.java
@@ -4,6 +4,7 @@
  */
 package calcolatrice_scrum;
 
+import java.lang.Math;
 /**
  *
  * @author Mattia Sanzari
@@ -52,5 +53,62 @@ public class Calcolatrice {
        }
        return risultato; 
     }
+   
+   public float conversioneDaRadiantiInGradi(float rad){
+       return  (float) Math.toDegrees(rad);
+   }
+   
+   public float conversioneDaGradiInRadianti(float gradi){
+       return  (float) Math.toRadians(gradi);
+   }
+   
+   
+    /*Funzione per la tangente valore in rad return in rad*/
+   public  float coseno(float valore){
+   
+    return (float) Math.cos(valore);
+   }
+   
+   
+   /* Funzione per il coseno (scegliendo se il valore è in gradi o radianti) */
+   public float coseno(float valore, boolean valoreInGradi){
+       if(valoreInGradi){
+           return (float) Math.cos(this.conversioneDaGradiInRadianti(valore));
+       } else {
+           // Se è già in radianti, uso direttamente il metodo base
+           return this.coseno(valore); 
+       }
+   }
+   
+   /* Funzione per il seno (valore in radianti) */
+   public float seno(float valore){
+       return (float) Math.sin(valore);
+   }
     
+   
+   /* Funzione per il seno (scegliendo se il valore è in gradi o radianti) */
+   public float seno(float valore, boolean valoreInGradi){
+       if(valoreInGradi){
+           return (float) Math.sin(this.conversioneDaGradiInRadianti(valore));
+       } else {
+           // Se è già in radianti, uso direttamente il metodo base
+           return this.seno(valore);
+       }
+   }
+   
+   
+   /* Funzione per la tangente (valore in radianti) */
+   public float tangente(float valore){
+       return (float) Math.tan(valore);
+   }
+   
+   /* Funzione per la tangente (scegliendo se il valore è in gradi o radianti) */
+   public float tangente(float valore, boolean valoreInGradi){
+       if (valoreInGradi){
+          return (float) Math.tan(this.conversioneDaGradiInRadianti(valore));
+       } else {
+          // Se è già in radianti, uso direttamente il metodo base
+          return this.tangente(valore);
+       }
+   }
 }
